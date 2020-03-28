f94032fc20d034f3870161e6a61e17c96acef10b
wyc1997 <wuyangchao1997@foxmail.com>
2020-03-28 13:50:49 -0700

    basic structure

---
+++ src/Client.js
@@ -0,0 +1,10 @@
+// define functions to communicate from frontend to backend
+
+
+
+
+
+
+
+const Client = {}; //remember to add your functions in {} to be able for it to be exported
+export default Client;
