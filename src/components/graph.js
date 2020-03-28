b165795ca6c18c1a30a8ac1808d2528edd82611b
wyc1997 <wuyangchao1997@foxmail.com>
2020-03-28 15:34:40 -0700

    added demo to canvajs graph

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import Client from '../Client'
-import canvasJS from 'canvasjs'
+import React from 'react';
+import Client from '../Client';
+import CanvasJSReact from './canvasjs.react';
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class MyGraph extends React.Component
 {
@@ -11,8 +14,27 @@ class MyGraph extends React.Component
         Client.retrieveGraphData(props.city, props.criteria, (res) => {
             graphData = res.data;
         });
+    }
 
+    render()
+    {
+        const options = {title: {text: "hello there"},
+                         data: [{
+                             type: "column",
+                             dataPoints: [
+                                 {label: "happy", y: 1},
+                                 {label: "happy", y: 2},
+                                 {label: "happy", y: 3},
+                                 {label: "happy", y: 4},
+                                 {label: "happy", y: 5},
+                             ]
+                         }]};
+        return (
+            <div>
+                <CanvasJSChart options = {options}/>
+            </div>
+        )
     }
 }
 
-export default {MyGraph};
+export default MyGraph;
