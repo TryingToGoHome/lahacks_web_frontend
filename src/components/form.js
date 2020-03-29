dec4dfce57bb64e58173988d6690bbf4779b6d15
wyc1997 <wuyangchao1997@foxmail.com>
2020-03-28 22:15:12 -0700

    added dropdown button to replace select

--- src/components/form.js
+++ src/components/form.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import Client from "../Client";
 import MyGraph from './graph';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'reactstrap';
+import {Button, ButtonDropdown, ButtonGroup, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 
 class GraphForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { city: "", criteria: "", data: {}, target:"", goal:""};
+        this.state = { city: "", criteria: "", data: {}, target:"", goal:"", isOpen:false};
+        this.available_cities = ["Los Angeles", "New York"]
     }
 
     myChangeHandler = event => {
@@ -20,19 +21,28 @@ class GraphForm extends Component {
         })
     };
 
+    toggle = () =>
+    {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+
     render() {
-        let cityOptions = [
-        <option key="first" value="LA">
-            LA
-        </option>,
-        <option key="second" value="NY">
-            NY
-        </option>
-        ];
+        let cityOptions = this.available_cities.map((city, idx) =>
+            <DropdownItem key={idx} value={city}>{city}</DropdownItem>
+        )
         let criteriaOptions = [
         <option key="first">5 days</option>,
         <option key="second">10 days</option>
         ];
+        var target;
+        if (this.state.target == "")
+        {
+            target = "Select a city";
+        }
+        else 
+        {
+            target = this.state.target;
+        }
         console.log(this.state);
         return (
         <div>
@@ -49,8 +59,17 @@ class GraphForm extends Component {
                 </option>
                 {criteriaOptions}
             </select>
+            <ButtonDropdown isOpen={this.state.isOpen} toggle={this.toggle}>
+                <Button id="caret" color="primary">{target}</Button>
+                <DropdownToggle caret color="primary" />
+                <DropdownMenu right>
+                    <DropdownItem header>Select a city</DropdownItem>
+                    <DropdownItem divider/>
+
+                </DropdownMenu>
+            </ButtonDropdown>
             <Button color="primary" type="submit" onClick={this.onSubmit}>
-            Submit
+                Submit
             </Button>
         </div>
         );
