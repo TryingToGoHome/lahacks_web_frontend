e5578b996b3b8e8e4b10a0447fee63e623c9ec33
wyc1997 <wuyangchao1997@foxmail.com>
2020-03-28 21:16:59 -0700

    added Jumbotron to background

--- src/components/background.js
+++ src/components/background.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Jumbotron} from 'reactstrap';
+import {Jumbotron} from 'reactstrap';
 import GraphForm from './form';
 
 class Background extends React.Component
